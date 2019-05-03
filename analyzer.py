90d6ef1499c66d7d2be242a53cd9596f22ed22cf
Alex <aleks.shutter@gmail.com>
2019-05-04 00:18:48 +0700

    Implemented Pearson's correlation

--- analyzer.py
+++ analyzer.py
@@ -1,9 +1,12 @@
 """Ok, let's face it - I suck at math, so it's likely that I messed
    up some widely known naming conventions and am doing stuff in a very
    naive childish way. I'm trying my best to avoid any kind of mistakes.
-   At the end of the day, what I care for is the essence, not the form."""
+   At the end of the day, what I care for is the essence, not the form.
 
-# This part is currently under construction.
+   I'm also aware that such things as numpy and scipy exist. I just wanna
+   try to figure this stuff out on my own.
+
+   P.S. This part is currently under construction."""
 
 class Analyzer():
 
@@ -26,27 +29,32 @@ class Analyzer():
 		else:
 			return arr[len(arr)//2]
 
-	def count_variance(self, array):
-		pass
-
-	def count_standard_deviation(self, samples):	# MESSY
+	def count_standard_deviation(self, samples):
 		mean = sum(samples)/len(samples)
 		variance = sum([(x-mean)**2 for x in samples])/len(samples)
 		standard_deviation = variance**0.5
+		return standard_deviation
 
-		print("Samples are:", samples)
-		print("Mean =", mean)
-		print("Variance =", variance)
-		print("SD =", standard_deviation)
+	def count_covariance(self, array_1, array_2):
+		raise NotImplementedError
 
-	def check_correlation_between(self, array_1, array_2):
-		pass
-		
+	def count_pearsons_correlation(self, array_1, array_2):				# check it
+		a = list(map(lambda x: x-sum(array_1)/len(array_1), array_1))
+		b = list(map(lambda x: x-sum(array_2)/len(array_2), array_2))
+		ab = list(map(lambda x, y: x*y, a, b))
+		a2 = list(map(lambda x: x*x, a))
+		b2 = list(map(lambda x: x*x, b))
+		return sum(ab) / (sum(a2)*sum(b2))**0.5
+
+	def count_spearmans_correlation(self, array_1, array_2):
+		raise NotImplementedError
+
+
 # - Create analytical system 
 # 	It should:
 # 		- give raw numbers (raw, max, min, avg, median)
 # 		- analyze each trackable's tendencies
-# 		- search for correlations in user's data
+# 		- search for relations in user's data (correlation / covariance)
 # 		- search for any relation between data and time (part of day / week / month / year)
 # 		- search for any other periodicality (see if there is a time pattern in data)
 # 		- ...?
