d1680ae5a0a8aaccec60d615044c3f10e98db7db
Alex <aleks.shutter@gmail.com>
2020-02-09 23:30:34 +0700

    Implemented proper argument parsing

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import sys
 import readline
+import argparse
 from views import home_view, missed_days_view, time_mgr, log_mgr
 
+parser = argparse.ArgumentParser(description='A program to log your daily routines.')
+parser.add_argument('-l', '--log', action='store_true', help='avoid unnecessary interaction - jump straight to logging')
 
 if __name__ == '__main__':
-    try:
-        if sys.argv[1] == "-l" or sys.argv[1] == "--log":
-            missed_days_view(time_mgr.get_all_missed_days(log_mgr))
-        else:
-            raise IndexError
-    except IndexError:
+    args = parser.parse_args()
+    if args.log:
+        missed_days_view(time_mgr.get_all_missed_days(log_mgr))
+    else:
         home_view()
